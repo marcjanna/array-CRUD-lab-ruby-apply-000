e60da5bc86bbaffe49c748e9d1e818b708d4cb36
Marcjanna Mietla <mietla.m@gmail.com>
2020-04-28 00:57:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,8 +20,8 @@ end
 
 def remove_element_from_end_of_array(array)
 array = [1,2,3,4,"arrays!"]
-array[4].to_i
-array.shift(array[4])
+array(4).to_i
+array.shift(array(4))
 end
 
 def remove_element_from_start_of_array(array)
