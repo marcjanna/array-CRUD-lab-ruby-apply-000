0df88e12c5f06b49a15edb28737c55a9a50a50f5
Marcjanna Mietla <mietla.m@gmail.com>
2020-04-28 01:24:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -41,5 +41,6 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  return array.last
 end
